b8629e34c6b3ad88a8c604b8a98d1e1c3e3aeb6c
Caius Durling <dev@caius.name>
2016-06-06 16:49:30 +0100

    Move overriding methods to their own module
    No point in including more than we have to when hooking into Gem::Installer

--- lib/rubygems/precompiled.rb
+++ lib/rubygems/precompiled.rb
@@ -12,11 +12,6 @@ require 'uri'
 require 'tempfile'
 
 module Precompiled
-  def self.included(base)
-    base.send(:alias_method, :build_extensions_without_cache, :build_extensions)
-    base.send(:alias_method, :build_extensions, :build_extensions_with_cache)
-  end
-
   GemCache = {
     'file' => FileCache,
     'http' => HttpCache
@@ -32,44 +27,51 @@ module Precompiled
     end
   end
 
-  def build_extensions_with_cache
-    spec = @package.spec
-    cache = Precompiled.precompiled_caches.find { |cache| cache.contains?(spec) }
-
-    if cache
-      $stderr.puts "Loading native extension from cache"
-      cache.retrieve(spec) do |path|
-        if spec.respond_to?(:extension_dir)
-          overlay_tarball(path, spec.extension_dir)
-        else
-          overlay_tarball(path, @gem_dir)
-        end
-      end
-    else
-      build_extensions_without_cache
+  module InstallerExtension
+    def self.included(base)
+      base.send(:alias_method, :build_extensions_without_cache, :build_extensions)
+      base.send(:alias_method, :build_extensions, :build_extensions_with_cache)
     end
-  end
 
-  # Private: Extracts a .tar.gz file on-top of the gem's installation directory
-  def overlay_tarball(tarball, target_root)
-    Zlib::GzipReader.open(tarball) do |gzip_io|
-      Gem::Package::TarReader.new(gzip_io) do |tar|
-        tar.each do |entry|
-          target_path = File.join(target_root, entry.full_name)
-          if entry.directory?
-            FileUtils.mkdir_p(target_path)
-          elsif entry.file?
-            FileUtils.mkdir_p(File.dirname(target_path))
-            File.open(target_path, "w") do |f|
-              f.write entry.read(1024*1024) until entry.eof?
-            end
+
+    def build_extensions_with_cache
+      spec = @package.spec
+      cache = Precompiled.precompiled_caches.find { |cache| cache.contains?(spec) }
+
+      if cache
+        $stderr.puts "Loading native extension from cache"
+        cache.retrieve(spec) do |path|
+          if spec.respond_to?(:extension_dir)
+            precompile_overlay_tarball(path, spec.extension_dir)
+          else
+            precompile_overlay_tarball(path, @gem_dir)
           end
-          entry.close
         end
-       end
+      else
+        build_extensions_without_cache
+      end
+    end
+
+    # Private: Extracts a .tar.gz file on-top of the gem's installation directory
+    def precompile_overlay_tarball(tarball, target_root)
+      Zlib::GzipReader.open(tarball) do |gzip_io|
+        Gem::Package::TarReader.new(gzip_io) do |tar|
+          tar.each do |entry|
+            target_path = File.join(target_root, entry.full_name)
+            if entry.directory?
+              FileUtils.mkdir_p(target_path)
+            elsif entry.file?
+              FileUtils.mkdir_p(File.dirname(target_path))
+              File.open(target_path, "w") do |f|
+                f.write entry.read(1024*1024) until entry.eof?
+              end
+            end
+            entry.close
+          end
+         end
+      end
     end
   end
-
 end
 
-Gem::Installer.send(:include, Precompiled)
+Gem::Installer.send(:include, Precompiled::InstallerExtension)
